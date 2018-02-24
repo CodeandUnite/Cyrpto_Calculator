247a302c1c2e2e8568eba7474aeba571b0939392
Austin Gomez <austincgomez@gmail.com>
2018-02-24 00:07:52 -0600

    Added testing. refactored code

--- view/core.py
+++ view/core.py
@@ -45,25 +45,21 @@ logo = """
 """
 version = "V0.1 Alpha5"
 
-def reroute_automatic():
-    ''' This will reroute the user whenever --help or --list are entered at
-    anytime for the duration of the program'''
 
 def input_sanitize_and_check_helper(tupleCheck):
-    from controller import processor
-    ''' This function will check the input that is entered. This is good to
-    ensure that no unexpected errors happen when the program is in opeeration.
-    This function will receive different variables under different conditions.
-    '''
-
-    ''' This is a central method of checking input that is inputted throughout
-    the program. Each command_code number signifies a different area of the program
-    that will be checked.
+    '''This function allows for all input to be sanitized throughout the
+    application. It is needed because we do not want any unsanitized data
+    going into the processor as it could mess up the input for the user.
+    The input sanitizer operates by a specific number that it receives. This
+    number tells it what to check for
 
     COMMAND NUMBER LIST:
     1 --> This will check the input for --help and --list within the calculator function
     2 --> This will check the input for --check within the calculator function
     3 --> This will check for --rise and --fall within the calculator function '''
+
+
+    from controller import processor
 
     command, cyrpto, fiat, command_code = tupleCheck
 
@@ -107,8 +103,23 @@ def input_sanitize_and_check_helper(tupleCheck):
 
 
 def help_view():
-    print "Welcome to the help view"
+    print "You have reached the help module"
+    print """Welcome to the help module for Cyrpto_Calculator.
+Cyrpto_Calculator is actively in the ALPHA stage of development. Therefore, if you encourted a bug within our program
+than we sincerely apologize for that! The best thing that you can do to help is to report bugs or suggest enhancements!
+By doing this, we can add your features and remove annoying bugs that we might have missed!
+Thank you for using this program """
 
+    userDecision = raw_input("Do you want to go back? Please type Yes or No exactly. Any other input will be interpreted as exiting the program")
+    if userDecision == "Yes":
+        print "Taking you back"
+        time.sleep(1)
+        home_screen_view()
+    if userDecision == "No":
+        help_view()
+    else:
+        print "Bye Bye"
+        sys.exit()
 def currency_list_view():
     print """
     cyrpto currencies available:
@@ -122,6 +133,9 @@ def currency_list_view():
 
           """
 def home_screen_view():
+    ''' The home_screen_view is the first view that the user will encounter.
+    Here it collects crucial input that the program needs and sends it to the
+    destination that the user defines '''
     print logo
     print version
     print "Welcome to Cyrptocalculator!"
@@ -145,6 +159,24 @@ def home_screen_view():
 
     default_check_boolean = False
 
+    if check_code == 0:
+        ''' The 0 check code will ensure that the -p command is accurate'''
+        verifyInfo = (command, cyrpto, fiat, check_code)
+        booleanJuryDecision = input_sanitize_and_check_helper(verifyInfo)
+        if booleanJuryDecision == True:
+            ''' The data is now safe to send to the calculator view.
+            The view should first go to a helper view to handle this exiting
+            home screen view '''
+            prepareCalcData = (command, cyrpto, fiat)
+            calculator_view(prepareCalcData)
+        if booleanJuryDecision == False or None:
+            print "We apologize but the data that was sent could not be verified."
+            print "Please make sure that you type your command exactly. If the problem persist please check the --help module"
+            print "Redirecting you to home_screen...."
+            time.sleep(2)
+            print "Loading your data....."
+            time.sleep(2)
+            home_screen_view()
 
     if check_code == 1:
         null_null = "null"
@@ -166,15 +198,7 @@ def home_screen_view():
             1. Need to define what the error it
             2. Must create a way to mitigate the error for the user '''
             print "An error occured!"
-    if check_code == 0:
-        verifyInfo = (command, cyrpto, fiat, check_code)
-        booleanJuryDecision = input_sanitize_and_check_helper(verifyInfo)
-        if booleanJuryDecision == True:
-            ''' The data is now safe to send to the calculator view.
-            The view should first go to a helper view to handle this exiting
-            home screen view '''
-            prepareCalcData = (command, cyrpto, fiat)
-            calculator_view(prepareCalcData)
+
 
 
 def calculator_view(calculatorData):
@@ -201,7 +225,56 @@ def calculator_view(calculatorData):
         dataTuple = (cyrptoCurrency, fiatCurrency)
         calculator_helper_two(dataTuple)
 
+def calculator_display_view(dataTuple):
+    '''calculator_display_view will display the statements that the calculator
+    helpers provide along with provide the user with options on what
+    to do next with the program'''
+    null_command, cyrpto, fiat, data_output = dataTuple
+    end_prog_boolean = False
+    calculator_required_data = (null_command, cyrpto, fiat)
+    print data_output
+    while end_prog_boolean == False:
+        print """Type the following commands to proceed with the program:
+             |--redirect calc or home | This will take you to the calculator or the home screen depending on what you select
+             |--end prog | This will end the program  """
+        user_input = raw_input("Please type your response here")
+        try:
+
+            commandVariable, directionVariable = user_input.split(" ")
+            if commandVariable == "--redirect":
+                if directionVariable == "calc":
+                    calculator_view(calculator_required_data)
+                    end_prog_boolean == True
+                elif directionVariable == "home":
+                    home_screen_view()
+                    end_prog_boolean == True
+                else:
+                    print "I am sorry but your input could not be understood"
+            if commandVariable == "--end":
+                if directionVariable == "prog":
+                    end_prog_boolean == True
+                    system.exit()
+                    time.sleep(1)
+                    print "Reinitizling....."
+                    time.sleep(2)
+            else:
+                print "Your input could not be understood"
+        except ValueError:
+            print "%s could not be understood. Please try again!"
+            time.sleep(1)
+            print "Reinitizling....."
+            time.sleep(2)
+
+
+
+
+
+
+
+
 def calculator_helper_one(lovely_data):
+    ''' calculator_helper_one will help the calculator process functions for
+    --rise and --fall within the calculator'''
     from model import bitcoin
     from controller import processor
     command, percentage, cyrpto, fiat = lovely_data
@@ -226,11 +299,18 @@ def calculator_helper_one(lovely_data):
             print equationReturn
 
 def calculator_helper_two(lovely_data):
+    '''
+    calculator_helper_two helps the calculator process the price check methods
+    for the program
+    '''
     from controller import processor
     user_cyrpto, user_fiat = lovely_data
     current_price_for_user = processor.handle_cyrpto_price_check(lovely_data)
-    print "The current price for %s is %d" % (user_cyrpto, current_price_for_user)
-    
+    data_output = "The current price for %s is %d" % (user_cyrpto, current_price_for_user)
+    null_command = "null"
+    NeccesaryDataPackage = (null_command, user_cyrpto, user_fiat, data_output)
+    calculator_display_view(NeccesaryDataPackage)
+
 
 
 
